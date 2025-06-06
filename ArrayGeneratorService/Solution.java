d251cf99fefbd972597a65822b6423e62bbf96f3
Walter Holley III <holley.walter@gmail.com>
2025-06-05 21:06:55 -0500

    Progressing through more optimized solution

--- ArrayGeneratorService/Solution.java
+++ ArrayGeneratorService/Solution.java
@@ -1,12 +1,9 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 import java.util.stream.IntStream;
 
 public class Solution {
 
-    public static List<Integer> getArray(List<Integer> numValues, String state, Integer m){
+    public static List<Integer> getArrayWithStream(List<Integer> numValues, String state, Integer m){
         ArrayList<Integer> result = new ArrayList<>();
         char[] states = state.toCharArray();
         int startIndex = state.indexOf('1');
@@ -24,14 +21,42 @@ public class Solution {
             System.out.println(processingStates);
 
             //update state
-            int nextUpdate = IntStream.range(0, states.length)
+            startIndex = IntStream.range(startIndex, states.length)
                     .filter(e -> (states[e] == '0' && e - 1 >=0 && states[e - 1] == '1'))
                     .findFirst()
                     .orElse(-1);
-            if(nextUpdate >= 0)
-                states[nextUpdate] = '1';
+            if(startIndex >= 0)
+                states[startIndex] = '1';
 
         }
+        return result;
+    }
+
+    public static List<Integer> getArray(List<Integer> numValues, String state, Integer m){
+        List<Integer> result = new ArrayList<>();
+        HashSet<Integer> availableStates = new HashSet<>();
+        char[] states = state.toCharArray();
+        int startIndex = 0;
+        boolean zeroFound = false;
+        for(int i = 0; i < m; i++){
+            int nextState = startIndex;
+
+            //manage state
+            for(int j = startIndex; j < state.length(); j++){
+                if(states[j] == '1')
+                    availableStates.add(j);
+                else if(states[j] == '0' && !zeroFound && j - 1 >= 0 && states[j - 1] == '1'){// update state
+                    nextState = j;
+                    zeroFound = true;
+                    states[j] = '1';
+
+                    if(i > 0)
+                        break;
+                }
+            }
+            //fin
+        }
+
         return result;
     }
 
@@ -41,7 +66,7 @@ public class Solution {
         String state = "0010011110";
         int m = 4;
 
-        List<Integer> result = getArray(arr, state, m);
+        List<Integer> result = getArrayWithStream(arr, state, m);
         System.out.println(result);
 
     }
